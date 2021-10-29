4cfaf11d7e6f19d87d1db3fddd672aa0c490c963
Nikita Krasavin <n.krasavin@inanomo.com>
2021-10-29 12:46:46 +0300

    players movement

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,45 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
-public class BallController : MonoBehaviour
+namespace Arkanoid
 {
-    [SerializeField]
-    private GameManager _gameManager;
-    [SerializeField]
-    private Rigidbody _rigidBody;
-    private Vector3 _oldVelocity;
-
-    void Start()
+    [RequireComponent(typeof(Rigidbody))]
+    public class BallController : MonoBehaviour
     {
-        var forceVector = new Vector3(0.2f, 0.1f, 1f);
-        _rigidBody.AddForce(forceVector * 500f);
-    }
+        [SerializeField]
+        private GameManager _gameManager;
+        [SerializeField]
+        private Rigidbody _rigidBody;
+        private Vector3 _oldVelocity;
 
-    void FixedUpdate()
-    {
-        _oldVelocity = _rigidBody.velocity;
-    }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        var contact = other.contacts[0];
-        var collidingObject = contact.otherCollider.gameObject;
-        Debug.Log("COLLISION " + collidingObject.name);
-        if (collidingObject.name == "LoseWall")
+        void Start()
         {
-            _gameManager.Loose();
+            var forceVector = new Vector3(0.2f, 0.1f, 1f);
+            _rigidBody.AddForce(forceVector * 500f);
         }
-        else if (collidingObject.name.Contains("Cube"))
+
+        void FixedUpdate()
         {
-            _rigidBody.velocity = Vector3.Reflect(_oldVelocity, contact.normal);
-            collidingObject.SetActive(false);
-            _gameManager.CheckWin();
+            _oldVelocity = _rigidBody.velocity;
         }
-        else
+
+        private void OnCollisionEnter(Collision other)
         {
-            _rigidBody.velocity = Vector3.Reflect(_oldVelocity, contact.normal);
+            var contact = other.contacts[0];
+            var collidingObject = contact.otherCollider.gameObject;
+            Debug.Log("COLLISION " + collidingObject.name);
+            if (collidingObject.name == "LoseWall")
+            {
+                _gameManager.Loose();
+            }
+            else if (collidingObject.name.Contains("Cube"))
+            {
+                _rigidBody.velocity = Vector3.Reflect(_oldVelocity, contact.normal);
+                collidingObject.SetActive(false);
+                _gameManager.CheckWin();
+            }
+            else
+            {
+                _rigidBody.velocity = Vector3.Reflect(_oldVelocity, contact.normal);
+            }
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,60 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class GameManager : MonoBehaviour
+namespace Arkanoid
 {
-    [SerializeField]
-    private GameObject[] _blocks;
-
-    public void Loose()
+    public class GameManager : MonoBehaviour
     {
-        Debug.Log("LOSE");
-    }
+        [SerializeField]
+        private GameObject[] _blocks;
+        [SerializeField]
+        private Rigidbody _ball;
+        [SerializeField]
+        private Rigidbody _playerOne;
+        [SerializeField]
+        private Rigidbody _playerTwo;
+        private Vector2 _playerOneVelocity;
+        private Vector2 _playerTwoVelocity;
 
-    public void CheckWin()
-    {
-        if (_blocks.Where(block => block.activeInHierarchy).Count() <= 0)
-            Debug.Log("WIN!");
+        private void FixedUpdate()
+        {
+            _playerOne.velocity = _playerOneVelocity * 6f;
+            _playerTwo.velocity = _playerTwoVelocity * 6f;
+
+            if (_playerOneVelocity.magnitude > 0.01)
+                _playerOneVelocity = _playerOneVelocity * 0.9f;
+            else
+                _playerOneVelocity = Vector2.zero;
+
+            if (_playerTwoVelocity.magnitude > 0.01)
+                _playerTwoVelocity = _playerTwoVelocity * 0.9f;
+            else
+                _playerTwoVelocity = Vector2.zero;
+        }
+
+        public void Loose()
+        {
+            Debug.LogWarning("LOSE");
+            _ball.velocity = Vector3.zero;
+        }
+
+        public void CheckWin()
+        {
+            if (_blocks.Where(block => block.activeInHierarchy).Count() <= 0)
+                Debug.LogWarning("WIN!");
+        }
+
+        public void OnMovePlayer1(InputAction.CallbackContext value)
+        {
+            Debug.Log("MOVE");
+            _playerOneVelocity += value.ReadValue<Vector2>();
+        }
+
+        public void OnMovePlayer2(InputAction.CallbackContext value)
+        {
+            var vector = value.ReadValue<Vector2>();
+            _playerTwoVelocity += new Vector2(-vector.x, vector.y);
+        }
     }
 }

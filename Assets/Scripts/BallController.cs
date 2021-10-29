6cda28ad2dac3c9027758f21cc3cdd3a2cafd68c
Nikita Krasavin <n.krasavin@inanomo.com>
2021-10-29 09:38:51 +0300

    add velocity to ball

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody _rigidBody;
+    private Vector3 _oldVelocity;
+
+    void Start()
+    {
+        var forceVector = new Vector3(0.2f, 0.1f, 1f);
+        _rigidBody.AddForce(forceVector * 500f);
+    }
+
+    void FixedUpdate()
+    {
+        _oldVelocity = _rigidBody.velocity;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        var contact = other.contacts[0];
+        _rigidBody.velocity = Vector3.Reflect(_oldVelocity, contact.normal);
+    }
+}

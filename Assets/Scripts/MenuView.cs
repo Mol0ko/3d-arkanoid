bf5eb67053b99b3b75d690ebab6dcdb184f6a1b1
Nikita Krasavin <n.krasavin@inanomo.com>
2021-11-10 09:01:08 +0300

    add menu scene

---
+++ Assets/Scripts/MenuView.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuView : MonoBehaviour
+{
+    #region SerializeField
+
+    [SerializeField]
+    private Text _versionText;
+
+    #endregion
+
+    #region Lifecycle
+
+    private void Awake() {
+        _versionText.text = "ver:" + Application.version;
+    }
+
+    #endregion
+
+    #region UI events
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPaused = true;
+#else
+        Application.Quit();
+#endif
+    }
+
+    #endregion
+}
